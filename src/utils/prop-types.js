00ff0bab490e0023092e6663144809a9db2ef2b7
son1s <fomin.ea2003@gmail.com>
2023-08-14 00:56:19 +0300

    fix: added prop-types

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,5 +1,35 @@
 import PropTypes from "prop-types";
 
 export const ingredientPropType = PropTypes.shape({
-  //укажите здесь prop-types для ингридиента
+  name: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
 });
+
+export const burgerConstructorPropTypes = {
+  top: PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  bottom: PropTypes.shape({
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  middle: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export const navLinkPropType = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['primary', 'inactive']),
+  href: PropTypes.string.isRequired,
+}
